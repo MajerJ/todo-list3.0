49dab13aece7516f1c0250a0a7d10d7bac9e5007
Jan Majer <33447429+MajerJ@users.noreply.github.com>
2018-01-06 18:00:59 +0100

    Add files via upload

---
+++ javascript/controllers/login.js
@@ -0,0 +1,69 @@
+angular.module('ToDo')
+.controller('LoginCtrl', function($scope, $http, $httpParamSerializerJQLike, $location) {
+    $scope.sessionUser = sessionStorage.getItem('user');    
+    $scope.user = ($scope.sessionUser !== null) ? JSON.parse($scope.sessionUser) : {};
+    $scope.reqUser = {};
+    
+    $scope.storeUser = function() {
+        sessionStorage.setItem('user', angular.toJson($scope.user));
+    }
+
+    $scope.login = function() {
+        $http({
+            method: 'POST',
+            url: 'http://localhost:3000/login',
+            data: $httpParamSerializerJQLike({username: $scope.reqUser.username, password: $scope.reqUser.password}),
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+            withCredentials: true
+        }).then(function(response) {
+            if (response.data.error) {
+                $scope.message = response.data.error;
+                $scope.reqUser = {};
+            } else {
+                $scope.user = response.data;
+                $scope.storeUser();
+                $scope.reqUser = {};
+                $location.path('/tasks');
+            }            
+        });
+    }
+
+    $scope.logout = function() {
+        $http({
+            method: 'GET',
+            url: 'http://localhost:3000/logout',
+            withCredentials: true
+        }).then(function(response) {
+            $scope.user = {};
+            $scope.storeUser();
+            $scope.reqUser = {};
+            $scope.message = response.data;                
+            $location.path('/login');     
+        });
+    }
+
+    $scope.register = function() {
+        $http({
+            method: 'POST',
+            url: 'http://localhost:3000/local-reg',
+            data: $httpParamSerializerJQLike({username: $scope.reqUser.username, password: $scope.reqUser.password}),
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+            withCredentials: true
+        }).then(function(response) {
+            if (response.data.error) {
+                $scope.message = response.data.error;
+                $scope.reqUser = {};
+            } else {
+                $scope.user = response.data;
+                $location.path('/tasks');
+            }            
+        });        
+    }
+
+    $scope.removeByCtrlD = function(keyEvent) {
+        if (keyEvent.which === 68 && keyEvent.ctrlKey) {
+            keyEvent.preventDefault();
+            $scope.$broadcast('remove');
+        }
+    };
+})
---
+++ javascript/routes.js
@@ -0,0 +1,16 @@
+angular.module('ToDo')
+.config(function($routeProvider){
+    $routeProvider.when('/login', {
+        templateUrl: '/pages/login.html'
+    })
+    .when('/register', {
+        templateUrl: '/pages/register.html'
+    })
+    .when('/tasks', {
+        templateUrl: '/pages/tasks.html'
+    })
+    .when('/', {
+        templateUrl: '/pages/login.html'
+    })
+    .otherwise({ redirectTo: '/' });
+});
